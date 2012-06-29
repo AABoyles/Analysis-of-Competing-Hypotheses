f3b6cfb8042eadb12e546e0ee38809badb0a092c
aboyles <ABoyles@milcord.com>
2012-06-29 11:50:50 -0400

    New variables in LocalSettings.

--- LocalSettings.php
+++ LocalSettings.php
@@ -2,9 +2,13 @@
 
 #This File is where all administrative settings should be configured.
 
+global $domain, $base_URL, $email_enabled, $email_address, $versionNumber, $dbhost, $dbusername, $dbuserpassword, $dbname;
+
 $domain="localhost";
-$base_URL="$domain/ACH";
-$email_domain="localhost";
+$base_URL="$domain/ach";
+
+$email_enabled=false;
+$email_address="admin@$domain";
 
 # You are configuring Analysis of Competing Hypotheses
 $versionNumber="1.0.3alpha";
@@ -12,5 +16,5 @@ $versionNumber="1.0.3alpha";
 # Database Settings:
 $dbhost = 'localhost';
 $dbusername = 'root';
-$dbuserpassword = '';
-$default_dbname = 'ach';
+$dbuserpassword = 'foo';
+$dbname = 'ach';
