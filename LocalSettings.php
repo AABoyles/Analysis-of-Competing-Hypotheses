7cf5c6e584a4b155e557504cc36a1bd31da4b171
Tony <AABoyles@gmail.com>
2012-07-01 13:13:56 -0400

    Rearranged settings in order of importance.

--- LocalSettings.php
+++ LocalSettings.php
@@ -4,17 +4,23 @@
 
 global $domain, $base_URL, $email_enabled, $email_address, $versionNumber, $dbhost, $dbusername, $dbuserpassword, $dbname;
 
+# Path Settings
 $domain="localhost";
-$base_URL="$domain/ach";
-
-$email_enabled=false;
-$email_address="admin@$domain";
-
-# You are configuring Analysis of Competing Hypotheses
-$versionNumber="1.0.3alpha";
+$path="ach";
 
 # Database Settings:
 $dbhost = 'localhost';
 $dbusername = 'root';
-$dbuserpassword = 'foo';
+$dbuserpassword = '';
 $dbname = 'ach';
+
+# Email Settings
+$email_enabled=false;
+$email_address="admin@$domain";
+
+
+# END USER CONFIGURATION OPTIONS.  DO NOT EDIT ANYTHING BEYOND THIS LINE.
+
+$base_URL="$domain/$path";
+
+$versionNumber="1.0.3alpha";
