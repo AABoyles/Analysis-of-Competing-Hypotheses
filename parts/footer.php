5cbb741d58b52310e646b055686b7971add138cb
Tony <AABoyles@gmail.com>
2012-07-01 12:54:52 -0400

    Refactored footer.

--- parts/footer.php
+++ parts/footer.php
@@ -23,8 +23,6 @@
 **    along with Open Source ACH. If not, see <http://www.gnu.org/licenses/>.
 //////////////////////////////////////////////////////////////////////////////// */
 
-
-
 /**
 * 
 * Pretty much just the link to the competinghypotheses.org and the About page.
@@ -32,36 +30,26 @@
 * For diagnostics, you can add load times to the bottom of the page by uncommenting from line 39 
 */
 
-?>
+echo '<div class="footer">';
 
+if( $print_mode ) {
+	if ($active_project->classification == 'U') {
+		$classificationBannerStyle = "unclassifiedPrintBanner"; } else
+	if ($active_project->classification == 'C') {
+		$classificationBannerStyle = "confidentialPrintBanner"; } else
+	if ($active_project->classification == 'S') {
+		$classificationBannerStyle = "secretPrintBanner"; } else
+	if ($active_project->classification == 'TS') {
+		$classificationBannerStyle = "topSecretPrintBanner"; }
 
+echo "<div class='$classificationBannerStyle'>
+<p>Overall Project Classification: <b>" . Project::classificationText($active_project->classification) . "</b></p></div>";
 
-<div class="footer">
+} 
 
+echo '<p>Based on <a href="http://competinghypotheses.org">The Open Source ACH Project</a> | <a href="about.php">About this instance</a></p>';
 
-
-<?php if( $print_mode ) { ?>
-
-<?php if ($active_project->classification == 'U') {
-			$classificationBannerStyle = "unclassifiedPrintBanner"; } else
-		if ($active_project->classification == 'C') {
-			$classificationBannerStyle = "confidentialPrintBanner"; } else
-		if ($active_project->classification == 'S') {
-			$classificationBannerStyle = "secretPrintBanner"; } else
-		if ($active_project->classification == 'TS') {
-			$classificationBannerStyle = "topSecretPrintBanner"; }
-
-
-echo "<div class=\"". $classificationBannerStyle . " projectClassificationPrintBanner\">";?>
-
-<p>Overall Project Classification: <b><?=Project::classificationText($active_project->classification)?></b></p>
-
-</div>
-
-<?php } ?>
-
-<p>Powered by <a href="http://competinghypotheses.org">The Open Source ACH Project</a> | <a href="http://competinghypotheses.org/reportABug">Report a bug</a> | <a href="/help/about.php">About this instance</a></p>
-
+/*
 <!-- UNCOMMENT THE REMAINDER OF THE FILE IF YOU WANT LOADING TIMES AND STATS TO APPEAR AT THE BOTTOM OF THE PAGE
 <?php 
 /*
@@ -78,6 +66,7 @@ $this_uri = $_SERVER['REQUEST_URI'];
 mysql_do("INSERT INTO load_times (url, load_time, queries, flag, user_id) VALUES ('$this_uri', '$LOAD_TIME', '$DB_QUERIES', '$flag', '$active_user->id');");
 
 if( $SPEED_REPORTING ) { */
+/*
 ?>
 
 <div style="color: #999999;">
@@ -101,5 +90,5 @@ if( $SPEED_REPORTING ) { */
 
 //print_r($SQL_STATEMENTS_CACHE)
 
-?>
--->
+--> 
+ */
