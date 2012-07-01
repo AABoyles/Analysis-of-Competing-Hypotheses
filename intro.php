4998e8e61f15d5124bdf40a8a7f141396732b491
Tony <AABoyles@gmail.com>
2012-07-01 11:25:20 -0400

    Fixed Broken LearnBoxes rendering

--- intro.php
+++ intro.php
@@ -1,27 +1,18 @@
 <div class="learnBoxes">
 
-	<h3>Learn about ACH</h3>
-		
-	<p>We strongly encourage you to <a href="<?=$base_URL?>help/index.php">read our help documents</a>. These pages fully explain the technique and all of the software's functionality.</p>
-	
+	<div class="learn">
+	<h3>Learn about ACH</h3>		
+	<p>We strongly encourage you to <a href="help/index.php">read our help documents</a>. These pages fully explain the technique and all of the software's functionality.</p>
 	</div>
 	
 	<div class="learn">
-	
 	<h3>Browse current projects</h3>
-		
-	<p><a href="<?=$base_URL?>projects/">View the public projects</a> currently active on this ACH instance.</p>
-	
+	<p><a href="projects/">View the public projects</a> currently active on this ACH instance.</p>
 	</div>
 
-<!--BRIDGE-SPECIFIC MATERIAL -->	
-		<div class="learn">
-	<h3>Start your own project</h3>
-		
-	<p><a href="<?=$base_URL?>project/new">Get right down to it</a>. </p>
-	
+	<div class="learn">
+	<h3>Start your own project</h3>	
+	<p><a href="project/new">Get right down to it</a>. </p>
 	</div>	
-<!--END BRIDGE-SPECIFIC MATERIAL -->	
-
 	
 </div>
