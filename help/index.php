628465b1c7d92f5d08dc9eeaef033b58fc2af1f9
Tony <AABoyles@gmail.com>
2012-07-17 22:39:48 -0400

    Refactored help file.

--- help/index.php
+++ help/index.php
@@ -2,78 +2,43 @@
 
 include("../code/includes.php");
 
-$username = $_REQUEST['username'];
-
 $display_user = new User();
-$display_user->populateFromAttribute($username, "username");
 
-?>
-
-<html>
-<head>
-	<title>ACH: Help</title>
-	<?php include("../parts/includes.php"); ?>
-	<script language="JavaScript" type="text/javascript">
-
-function changeFontSize(inc)
-
-{
-
-  var p = document.getElementsByTagName('p');
-
-  for(n=0; n<p.length; n++) {
-
-    if(p[n].style.fontSize) {
-
-       var size = parseInt(p[n].style.fontSize.replace("px", ""));
-
-    } else {
-
-       var size = 14;
-
-    }
-
-    p[n].style.fontSize = size+inc + 'px';
-
-   }
-
+if(array_key_exists('username', $_REQUEST)){
+	$username = $_REQUEST['username'];
+	$display_user->populateFromAttribute($username, "username");
 }
 
+echo '<!DOCTYPE HTML><html><head><title>ACH: Help</title>';
+
+include(__DIR__ . "/../parts/includes.php");
+
+echo '<script language="JavaScript" type="text/javascript">
+function changeFontSize(inc)
+{
+  var p = document.getElementsByTagName(\'p\');
+  for(n=0; n<p.length; n++) {
+    if(p[n].style.fontSize) {
+       var size = parseInt(p[n].style.fontSize.replace("px", ""));
+    } else {
+       var size = 14;
+    }
+    p[n].style.fontSize = size+inc + \'px\';
+   }
+}
 </script>
-</head>
+</head>';
 
-<?php include("../parts/header.php"); ?>
-
-
-
-
-
-
-
-<?php
+include(__DIR__ . "/../parts/header.php"); 
 	
-if( $active_user->logged_in ) { ?>
-	
+if( $active_user->logged_in ) {
+	include(__DIR__ . "/../parts/menu_sidebar.php");
+} else {
+	include(__DIR__ . "/../parts/login_sidebar.php");
+	echo '</div>';
+}
 
-
-<?php include("../parts/menu_sidebar.php"); ?>
-
-<?php } else { ?>
-
-
-
-<?php include("../parts/login_sidebar.php"); ?>
-
-
-
-
-
-</div>
-
-<?php } ?>
-
-
-
+echo '
 <div class="mainContainer">
 
 <div class="ydsf left"><div class="inner">
@@ -90,44 +55,14 @@ if( $active_user->logged_in ) { ?>
 	<p><a href="help/rate_consistency.php">Rating the Consistency of Your Evidence</a></p>
 	
 	<h3>The ACH Software</h3>
-	<p><a href="help/howto_collaborate.php">ACH's Collaboration Tools</a></p>
+	<p><a href="help/howto_collaborate.php">ACH\'s Collaboration Tools</a></p>
 	<p><a href="help/howto_hypotheses.php">Entering and Editing Hypotheses</a></p>
 	<p><a href="help/howto_evidence.php">Entering and Editing Evidence and Arguments</a></p>	
 	<p><a href="help/howto_matrices.php">Working With Matrices</a></p>
 	<p><a href="help/howto_project_management.php">Project Management</a></p>
 
+</div></div></div></div></div></div></div>';
 
+include("../parts/footer.php");
 
-
-
-
-</div></div>
-
-</div>
-
-
-
-
-
-
-
-</div>
-
-</div></div>
-
-</div>
-
-
-
-
-
-
-
-<?php include("../parts/footer.php"); ?>
-
-
-
-
-
-</body>
-</html>
+echo '</body></html>';
