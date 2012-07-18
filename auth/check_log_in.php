9db328c902b24f4fa572e30c56b4f03802b5bacf
Tony <AABoyles@gmail.com>
2012-07-18 00:14:36 -0400

    Logins finally being checked correctly.

--- auth/check_log_in.php
+++ auth/check_log_in.php
@@ -4,24 +4,32 @@ echo '<!DOCTYPE html><html><head><title>Checking Log In...</title>';
 
 include (__DIR__."/../code/includes.php");
 
-$cookie_user_username = $_REQUEST['cookie_user_username'];
-$cookie_user_password = $_REQUEST['cookie_user_password'];
+if(! (array_key_exists('cookie_user_username', $_POST) || 
+	  array_key_exists('cookie_user_password', $_POST))){
+	  	die(); }
+
+$cookie_user_username = $_POST['cookie_user_username'];
+$cookie_user_password = $_POST['cookie_user_password'];
 
 $success = FALSE;
 $result = mysql_do("SELECT password FROM users WHERE username='$cookie_user_username' LIMIT 1;");
-while ($query_data = mysqli_fetch_array($result) !== NULL) {
-	if ($query_data['password'] == md5($cookie_user_password)) {
-		$success = TRUE;
-	} else {
-		$failedlogin = "That username/password combination is not correct";
-	}
+$query_data = mysqli_fetch_array($result, MYSQLI_NUM);
+
+if ($query_data !== NULL) {
+	echo "Queried Password: ".$query_data[0] . "<br>";
+	echo "POSTed Password:  ".md5($cookie_user_password);
+	
+	if (strcmp($query_data[0], md5($cookie_user_password)) == 0) {
+		$success = TRUE;}
+	else {
+		$failedlogin = "That username/password combination is not correct"; }
 }
 
 #$goto = $_REQUEST['goto'] . $active_user -> users[0];
 
-if ($success) {
+if ($success === TRUE) {
 	$active_user = new User();
-	$active_user -> populateFromAttribute($_POST['cookie_user_username'], "username");
+	$active_user -> populateFromUsername($_POST['cookie_user_username']);
 	$active_user -> setCookies();
 	User::successfulLogin();
 	//print_r($active_user);
