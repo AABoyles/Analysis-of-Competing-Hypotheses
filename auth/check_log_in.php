a85a5add7d512aab05a0bf23c1758d945ead20b1
Tony <AABoyles@gmail.com>
2012-07-19 01:21:49 -0400

    Fixing login checking.

--- auth/check_log_in.php
+++ auth/check_log_in.php
@@ -8,18 +8,17 @@ if(! (array_key_exists('cookie_user_username', $_REQUEST) ||
 	  array_key_exists('cookie_user_password', $_REQUEST))){
 	  	die(); }
 
-$cookie_user_username = $_REQUEST['cookie_user_username'];
-$cookie_user_password = $_REQUEST['cookie_user_password'];
+$cookie_user_username =     $_REQUEST['cookie_user_username'];
+$cookie_user_password = md5($_REQUEST['cookie_user_password']);
 
 $success = FALSE;
 $result = achquery("SELECT password FROM users WHERE username='$cookie_user_username' LIMIT 1;");
-$query_data = mysqli_fetch_array($result, MYSQLI_NUM);
+$query_data = mysqli_fetch_array($result, MYSQLI_BOTH);
 
 if ($query_data !== NULL) {
-	echo "Queried Password: ".$query_data[0] . "<br>";
-	echo "POSTed Password:  ".md5($cookie_user_password);
-	
-	if (strcmp($query_data[0], md5($cookie_user_password)) == 0) {
+/*	echo "Queried Password: ".$query_data[0] . "<br>";
+	echo "POSTed Password:  ".$cookie_user_password;*/
+	if (strcmp($query_data[0], $cookie_user_password) == 0) {
 		$success = TRUE;}
 	else {
 		$failedlogin = "That username/password combination is not correct"; }
@@ -31,16 +30,18 @@ if ($success === TRUE) {
 	$active_user = new User();
 	$active_user -> populateFromUsername($_REQUEST['cookie_user_username']);
 	$active_user -> setCookies();
-	User::successfulLogin();
-	//print_r($active_user);
-	} 
+	User::successfulLogin();}
 else {
 	User::failedLogin();}
+
+$goto = '../index.php';
 
 if (isset($_REQUEST['goto'])) {
 	$goto = $_REQUEST['goto'] . $active_user -> users[0];}
 
-echo "<meta http-equiv=Refresh content='0; url=../index.php'>";
+echo "<meta http-equiv=Refresh content='0; url=$goto'>";
+
 include (__DIR__."/../parts/includes.php");
+
 echo "<script language='JavaScript'>createCookie('cachchat', 'y', 7);</script>
-	  </head><body></body></html>";
+</head><body></body></html>";
