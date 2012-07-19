89904464880833cff97ca35c1e230576968cfd0b
Tony <AABoyles@gmail.com>
2012-07-18 23:13:47 -0400

    Still Tightening up Security a la User password dialogues...

--- password_reset.php
+++ password_reset.php
@@ -1,17 +1,7 @@
 <?php
-include("parts/includes.php");
 include("code/includes.php");
 
 $user_found = false;
-if( isset($_REQUEST['submit']) ) {
-	$this_user = new User();
-	$this_user->getAttr($_REQUEST['email'], "email");
-	if( $this_user->id > 0 ) {
-		$user_found = true;
-		$this_user->sendPasswordReset();
-		//sendMail($this_user->email, "[CACH] Password reset request", "Please reset your password by visiting this URL:\r\n\r\n " . $base_URL . "password_reset/" . $this_user->username . "/" . md5($this_user->password) . "\r\n\r\nThanks!\r\n\r\n - The ACH Bot");
-	}
-}
 
 echo '<!DOCTYPE html><html><head><title>ACH: Reset Your Password</title>';
 include("parts/includes.php");
@@ -19,8 +9,14 @@ echo '</head><body>';
 include("parts/header.php");
 echo '<div class="mainContainer"><h2>Forgotten Password Recovery</h2>';
 
-if( $user_found ) {
-	echo '<p>Okay &mdash; we\'ve sent you an e-mail with instructions for reseting your password.</p>';
+if( isset($_REQUEST['email']) ) {
+	$this_user = new User();
+	$this_user->getAttr($_REQUEST['email'], "email");
+	if( $this_user->id > 0 ) {
+		$user_found = true;
+		$this_user->sendPasswordReset();
+		//sendMail($this_user->email, "[CACH] Password reset request", "Please reset your password by visiting this URL:\r\n\r\n " . $base_URL . "password_reset/" . $this_user->username . "/" . md5($this_user->password) . "\r\n\r\nThanks!\r\n\r\n - The ACH Bot");
+		echo "<p>Okay &mdash; we've sent you an e-mail with instructions for reseting your password.</p>";}
 } else {
 	if( isset($_REQUEST['submit']) ) {
 		echo '<p class="error">Oops. We can\'t find that e-mail address in our system.</p>';
--- password_reset_change.php
+++ password_reset_change.php
@@ -1,36 +1,28 @@
 <?php
 
 include("code/includes.php");
-
-$user_found_with_password = false;
-
-if( isset($_REQUEST['username']) ) {
-	$this_user = new User();
-	$this_user->getAttr($_REQUEST['username'], "username");
-	if( md5($this_user->password) == $_REQUEST['password_hash'] ) {
-		$user_found_with_password = true;
-	}
-}
-
 echo '<!DOCTYPE html><html><head><title>Change Your Password</title>';
 include("parts/includes.php");
 echo '</head><body>';
 include("parts/header.php");
 echo '<div class="mainContainer"><h2>Password Reset</h2>';
 
-if( $user_found_with_password ) {
-	echo '<p>Please enter and confirm your password below:</p>
-<form method="post" action="/password_reset/action">
+if( isset($_REQUEST['username']) ) {
+	$this_user = new User();
+	$this_user->getAttr($_REQUEST['username'], "username");
+	if( strcmp( $this_user->password, $_REQUEST['password_hash'] ) == 0) {
+		echo '
+<p>Please enter and confirm your password below:</p>
+<form method="post" action="/password_reset.php">
 <input type="hidden" value="'.$_REQUEST['username'].'" name="username" / >
 <input type="hidden" value="'.$_REQUEST['password_hash'].'" name="password_hash" / >
 <p><b>Password:</b> <input type="password" size="15" name="password" / ></p>
 <p><b>Confirm Password:</b> <input type="password" size="15" name="password_2" / ></p>
 <p><input type="submit" name="submit" value="Change Password" />
-</form>';
-} else {
-	echo '<p>Account not found.</p>';
+</form>';} #TODO: Validate Password equality with Javascript on the spot.
+else {
+	echo '<p>Account not found.</p>';}
 }
-
 echo '</div>';
 include("parts/footer.php");
 echo '</body></html>';
